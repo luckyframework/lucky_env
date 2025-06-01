d83d7e3269c5d72367a373ff71db37ed1fe9baff
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-06-01 15:40:42 +0000

    Implement type-safe method defintions for env variables #38  (#39)
    * feat: implement method generation for env variables
    
    * feat: update `init_env` tests
    
    * doc: update init_env
    
    * test: update `init_env` tests
    
    * fix: `init_env` prints compiler warning if crystal version is <1.16.0

---
+++ spec/init_env_spec.cr
@@ -0,0 +1,22 @@
+require "./spec_helper"
+
+# Must be called up here, otherwise crystal throws "Error: can't declare def dynamically" error
+LuckyEnv.init_env("./spec/support/.env")
+
+describe "loading of env variables as method definitions" do
+  it "loads the ENV as method definitions" do
+    LuckyEnv.load("./spec/support/.env")
+
+    LuckyEnv.asset_host.should eq "https://luckyframework.org"
+    LuckyEnv.app_name.should eq "my_app"
+    LuckyEnv.lucky_env.should eq "development"
+    LuckyEnv.enable_cache?.should eq true
+    LuckyEnv.dev_port.should eq 3500
+    LuckyEnv.secret_key_base.should eq "j5I0VrpzT1Of7dhCA="
+    LuckyEnv.env_with_space.should eq "start_end"
+    LuckyEnv.db_name.should eq "my_app_development"
+    LuckyEnv.literal.should eq "${NOT_EXISTS_ENV}"
+    LuckyEnv.exchange_rate.should eq 2.34
+    LuckyEnv.host.should eq "0.0.0.0"
+  end
+end
--- src/lucky_env.cr
+++ src/lucky_env.cr
@@ -11,6 +11,85 @@ module LuckyEnv
     end
   end
 
+  # Generates type-safe method definitions for env variables.
+  #
+  # Usage:
+  #   `LuckyEnv.init_env` or
+  #   `LuckyEnv.init_env "/path/to/env-file"`
+  #
+  # Env File:
+  # ```
+  # LUCKY_ENV    = development
+  # DEV_PORT     = 3500
+  # ENABLE_CACHE = true
+  # ```
+  #
+  # Output:
+  # ```
+  # def LuckyEnv.lucky_env : String
+  #   ENV["LUCKY_ENV"]
+  # end
+  #
+  # def LuckyEnv.dev_port : Int32
+  #   ENV["DEV_PORT"].to_i
+  # end
+  #
+  # def LuckyEnv.enable_cache? : Bool
+  #   ENV["enable_cache"] == "true"
+  # end
+  # ```
+  #
+  macro init_env(path = ".env")
+    {% if compare_versions(Crystal::VERSION, "1.16.0") >= 0 %}
+      {% env_data = read_file?(path) %}
+
+      {% if env_data.nil? %}
+        {{ warning("Skipping generating method definitions for '#{path.id}'. File is empty or does not exits. Did you forget to create it?") }}
+      {% else %}
+
+        {% regex = /(?<key>[\w\d_]+)=(?<value>.*)/ %}
+
+        {% for line in env_data.lines %}
+          {% line = line.strip.chomp %}
+          {% if !line.starts_with?("#") && !line.empty? %}
+            {% hash, str = line.scan(/(?<key>[\w\d\_]+)=(?<value>.*)/) %}
+
+            {% if !hash.nil? %}
+              {% value = hash["value"] %}
+              {% def_name = hash["key"].downcase %}
+              {% type = "String" %}
+
+              {% if value =~ /^\d+$/ %}
+                {% type = "Int32" %}
+              {% elsif value =~ /^\d+\.\d+$/ %}
+                {% type = "Float64" %}
+              {% elsif value =~ /^true|false$/ %}
+                {% type = "Bool" %}
+                {% def_name += "?" %}
+              {% end %}
+
+              {% key = hash["key"] %}
+              def LuckyEnv.{{ def_name.id }} : {{ type.id }}
+                {% if type == "Bool" %}
+                  ENV[{{ key }}] == "true"
+                {% elsif type == "Int32" %}
+                  ENV[{{ key }}].to_i
+                {% elsif type == "Float64" %}
+                  ENV[{{ key }}].to_f
+                {% else %}
+                  ENV[{{ key }}].as({{ type.id }})
+                {% end %}
+              end
+            {% end %}
+
+          {% end %}
+        {% end %}
+      {% end %}
+    {% else %}
+      {{ warning("LuckyEnv.init_env requires crystal >= 1.16.0. Skipping method generation...") }}
+    {% end %}
+  end
+
   # Parses the `file_path`, and loads the results in to `ENV`
   # raises `LuckyEnv::MissingFileError` if the file is missing
   def self.load(file_path : String) : Hash(String, String)
